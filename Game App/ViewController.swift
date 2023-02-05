473a63b68769c7e379657dfa6c4394b9af9ff291
ahfasxp1 <ahfas.fazria@gmail.com>
2023-02-05 08:34:29 +0700

    Initial Commit

---
+++ Game App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game App
+//
+//  Created by OjekBro - Ahfas on 05/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
